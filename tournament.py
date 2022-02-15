a64dfee9ae8bf1d3335e942b8c5469b19bbc3029
Kamil <kamil.p.kubiak@gmail.com>
2022-02-15 21:30:02 +0100

    Create db, add items to db

--- tournament.py
+++ tournament.py
@@ -2,9 +2,24 @@
 #pip install python-dotenv
 #set FLASK_APP=tournament.py
 #flask run
+#pip install flask-sqlalchemy
+#creating db in python terminal: python, from tournament import db, db.create_all()
+#adding elements in console to db: from tournament import Item, item1 = Item(name="Qamas", surname="Q", points=6) , db.session.add(item1), db.session.commit
+#checking items in db: Item.query.all()
+from operator import length_hint
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tournament.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=False)
+    surname = db.Column(db.String(length=30), nullable=False, unique=False)
+    points = db.Column(db.Integer(), nullable=False)
+
 
 @app.route('/')
 @app.route('/home')
