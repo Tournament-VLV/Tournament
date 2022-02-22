700a2dc133fbe8efd84cca2fed9623b35d25d54e
Kamil <kamil.p.kubiak@gmail.com>
2022-02-22 19:59:24 +0100

    Added model relationships

--- tournament/models.py
+++ tournament/models.py
@@ -1,10 +1,21 @@
+from email.policy import default
 from tournament import db
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), unique=True, nullable=False)
+    email_address = db.Column(db.String(length=50), unique=True, nullable=False)
+    user_password = db.Column(db.String(length=70), unique=False, nullable=False)
+    user_points = db.Column(db.Integer(), nullable=True, default=0)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=False)
     surname = db.Column(db.Integer(), nullable=False)
-    points = db.Column(db.String(length=12), nullable=False, unique=True)
+    points_on_tournament = db.Column(db.Integer(), nullable=True, unique=False, default=0)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
 
     def __repr__(self):
         return f'Item {self.name}'
