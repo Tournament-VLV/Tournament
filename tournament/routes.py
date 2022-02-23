acde73588afc61101e341edba10018c63a33f989
Kamil <kamil.p.kubiak@gmail.com>
2022-02-23 21:20:49 +0100

    POST data to db, register form

--- tournament/routes.py
+++ tournament/routes.py
@@ -1,8 +1,10 @@
 from re import I
 from tournament import app
-from flask import render_template
-from tournament.models import Item
+from flask import render_template, redirect, url_for
+from tournament.models import Item, User
 from tournament.forms import RegisterForm
+from tournament import db
+
 
 @app.route('/')
 @app.route('/home')
@@ -14,7 +16,14 @@ def stats():
     items = Item.query.all()
     return render_template('stats.html', items=items) 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, email_address=form.email_address.data, user_password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('stats'))
     return render_template('register.html', form=form)
+
+    
